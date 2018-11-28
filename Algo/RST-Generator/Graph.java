6ed75c641df6aa2836d68ec11f39d3e8d9e0145b
killian05000 <killianwolfger@hotmail.fr>
2018-11-28 08:19:52 +0100

    update

---
+++ Algo/RST-Generator/Graph.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Graph implements Iterable<Edge>{
+    // classe de graphe non orientés permettant de manipuler
+    // en même temps des arcs (orientés)
+    // pour pouvoir stocker un arbre couvrant, en plus du graphe
+    
+	int order;
+	int edgeCardinality;
+	
+	ArrayList<LinkedList<Edge>> adjacency;
+	ArrayList<LinkedList<Arc>> inAdjacency;
+	ArrayList<LinkedList<Arc>> outAdjacency;
+	
+	public boolean isVertex(int index) {
+		return false;
+	    // à remplir
+	}
+	
+	public <T> ArrayList<LinkedList<T>> makeList(int size) {
+		ArrayList<LinkedList<T>> res = new ArrayList<>(size);
+		for(int i = 0; i <= size; i++) {
+			res.add(null);			
+		}
+		return res;
+	}
+	
+	public Graph(int upperBound) {
+	    // à remplir
+	}
+	
+	public void addVertex(int indexVertex) {
+	    // à remplir
+	}
+	
+	public void ensureVertex(int indexVertex) {
+	    // à remplir
+	}	
+	
+	public void addArc(Arc arc) {
+	    // à remplir
+	}
+	
+	public void addEdge(Edge e) {
+	    // à remplir
+	}
+
+	@Override
+	public Iterator<Edge> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object outNeighbours(int sommet) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
